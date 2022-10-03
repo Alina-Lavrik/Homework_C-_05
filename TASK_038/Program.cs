e1871a470b6c6b8d4188d8c8db06d2fdd6bd849d
Alina Lavrik <alina_voro@mail.ru>
2022-10-03 22:41:21 +0300

    Add Task 38_разница между макс и мин элементами массива

---
+++ TASK_038/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+int[] array = GetArray(6, 1, 100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($"Разница = {GetDifference(array)}");
+
+int GetDifference(int[] array)
+{
+    int Maxnum = array[0];
+    int Minnum = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > Maxnum)
+        {
+            Maxnum = array[i];
+        }
+        else if (array[i] < Minnum)
+        {
+            Minnum = array[i];
+        }
+
+    }
+    Console.WriteLine($"Максимальный элемент = {Maxnum} и минимальный элемент = {Minnum}");
+    return Maxnum - Minnum;
+}
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
